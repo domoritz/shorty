19930a88c000a09812013481017b1b88593caeea
Fritz Lekschas <service@lekschas.de>
2018-09-22 15:27:40 -0400

    Supported more bib styles and shorted doi URLs too (#1)
    * Diversify supported bib styles and shorted doi URLs too
    
    * Shoritfy URLs even further!
    
    * Remove unnecessary code
    
    * Make distinction between the doi and url entry more clear

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ import re
 import urllib.request as request
 import sys
 
+regex_doi = r"doi\s?=\s?[{\"']([^}\"']+)[}\"']"
+regex_url = r"url\s?=\s?[{\"'](https?://doi\.org/([\w\.]+/[^}/\"']+))/?[}\"']"
+
 
 for line in fileinput.input():
     try:
-        if "doi={" in re.sub("\s+", "", line).lower():
-            doi = re.search("{(.+)}", line).group(1)
+        search_doi = re.search(regex_doi, re.sub("\s+", "", line), re.IGNORECASE)
+        search_url = re.search(regex_url, re.sub("\s+", "", line), re.IGNORECASE)
+        doi = (
+            (search_doi and search_doi.group(1)) or
+            (search_url and search_url.group(2))
+        )
+
+        if doi:
+            short_doi = data["shortDOI"][3:] if search_url else data["shortDOI"]
             data = json.load(request.urlopen("http://shortdoi.org/%s?format=json" % doi))
-            print(line.replace(doi, data["ShortDOI"]), end="")
+            print(line.replace(doi, short_doi, end="")
         else:
             print(line, end="")
     except Exception as err:
